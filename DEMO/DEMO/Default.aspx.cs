b4593ea9191beb3e48cecfd11f3e4f1d3005dead
Harish Bansal <harishchief@gmail.com>
2011-11-17 22:07:23 -0800

    demo project

---
+++ DEMO/DEMO/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DataAnnotation;
+
+namespace DEMO
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Model.ContactInfo oContactInfo = new DEMO.Model.ContactInfo
+            {
+                FirstName = "Harish",
+                LastName = "Bansal",
+                Email = "harish@gmail.com",
+                Mobile = 4515733426,
+                URL = "http://www.google.com",
+                Birthday = Convert.ToDateTime("12/14/2001")
+            };
+
+            DataAnnotation.ModelValidate modelState = new ModelValidate();
+
+            if (modelState.Validate<Model.ContactInfo>(oContactInfo))
+            {
+                labelMessage.Text = "Form has been validated";
+            }
+            else
+                labelMessage.Text = modelState.GetError(",");
+        }
+    }
+}
---
+++ DEMO/DEMO/Model/ContactInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DataAnnotation;
+
+namespace DEMO.Model
+{
+    public class ContactInfo
+    {
+        [ValidationType(Validation = ValidationType.Required, ErrorMessage = "First Name is required")]
+        [ValidationType(Validation = ValidationType.MaxLength, MaxLength = 50)]
+        public string FirstName { get; set; }
+
+        [ValidationType(Validation = ValidationType.MaxLength, MaxLength = 50)]
+        public string LastName { get; set; }
+
+        [ValidationType(Validation = ValidationType.Required)]
+        [ValidationType(Validation = ValidationType.Range, MinRange = 1000000000, MaxRange = 9999999999)]
+        [ValidationType(Validation = ValidationType.MaxLength, MaxLength = 10)]
+        public Int64 Mobile { get; set; }
+
+        [ValidationType(Validation = ValidationType.Required)]
+        [ValidationType(Validation = ValidationType.MaxLength, MaxLength = 40)]
+        [ValidationType(Validation = ValidationType.Email)]
+        public string Email { get; set; }
+
+        [ValidationType(Validation = ValidationType.Required)]
+        [ValidationType(Validation = ValidationType.CustomExpression, RegularExpression = "([1-9]|0[1-9]|1[012])[- /.]([1-9]|0[1-9]|[12][0-9]|3[01])[- /.][0-9]{4}")]
+        public DateTime Birthday { get; set; }
+
+        [ValidationType(Validation = ValidationType.Required)]
+        [ValidationType(Validation = ValidationType.Url)]
+        public string URL { get; set; }
+    }
+}
